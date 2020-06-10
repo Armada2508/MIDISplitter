430cef5440ab30bbb3e7b75cc17ce9ca9c84bb93
Ben Abbott <babbott1000@gmail.com>
2020-06-10 17:10:52 -0500

    Added comments, added clipping to GUI

--- gui.py
+++ gui.py
@@ -30,6 +30,7 @@ class Interface(tk.Frame):
 		self.pack()
 		# Create the main gui with widgets
 		self.create_gui()
+		# Run the gui
 		tk.mainloop()
 
 	def select_input(self):
@@ -48,48 +49,66 @@ class Interface(tk.Frame):
 			# Set the output file
 			self.output_file.set(self.output_file_chooser)
 
+	# Create the main gui with widgets
 	def create_gui(self):
-		# Create the main gui with widgets
-		self.info = tk.Frame(self.master, width=0, height=80, bg="red")
-		self.input = tk.Frame(self.master, width=0, height=80, bg="orange")
-		self.output = tk.Frame(self.master, width=0, height=80, bg="yellow")
-		self.options = tk.Frame(self.master, width=0, height=80, bg="brown")
-		self.velocity = tk.Frame(self.options, width=0, height=0, bg="blue")
-		self.tracks = tk.Frame(self.options, width=0, height=0, bg="green")
-		self.convert = tk.Frame(self.master, width=0, height=80, bg="purple")
+		# Initialize Frame objects for containing widgets
+		self.info = tk.Frame(self.master, width=0, height=80)
+		self.input = tk.Frame(self.master, width=0, height=80)
+		self.output = tk.Frame(self.master, width=0, height=80)
+		self.options = tk.Frame(self.master, width=0, height=80)
+		self.velocity = tk.Frame(self.options, width=0, height=0)
+		self.clipping = tk.Frame(self.options, width=0, height=0)
+		self.convert = tk.Frame(self.master, width=0, height=100)
+		# Create the main title
 		self.title = tk.Label(self.info, text="MIDI Splitter", font=self.font_large, justify="center", width=10, border=0)
+		# Create an input and output button which brings up a dialog for selecting files
 		self.input_button = tk.Button(self.input, text="Input MIDI File", font=self.font_small, command=self.select_input, relief="groove", borderwidth=2)
 		self.output_button = tk.Button(self.output, text="Output MIDI File", font=self.font_small, command=self.select_output, relief="groove", borderwidth=2)
+		# Create the main convert button
 		self.convert_button = tk.Button(self.convert, text="Convert", font=self.font_medium, command=self.convert_song, relief="groove", borderwidth=2)
+		# Create string variables so that values can be accessed that are typed into entries
 		self.input_file = tk.StringVar()
 		self.output_file = tk.StringVar()
 		self.note_velocity = tk.StringVar()
+		self.clipping_margin = tk.StringVar()
+		# Create entries for files and options
 		self.input_file_entry = tk.Entry(self.input, font=self.font_small, justify="center", textvariable=self.input_file)
 		self.output_file_entry = tk.Entry(self.output, font=self.font_small, justify="center", textvariable=self.output_file)
 		self.velocity_entry = tk.Entry(self.velocity, font=self.font_small, justify="center", textvariable=self.note_velocity)
-		self.velocity_label = tk.Label(self.velocity, font=self.font_small, text="Change All Note Velocity(Volume)")
+		self.clipping_entry = tk.Entry(self.clipping, font=self.font_small, justify="center", textvariable=self.clipping_margin)
+		# Create labels for options
+		self.velocity_label = tk.Label(self.velocity, font=self.font_small, text="Change All Note Velocity(Volume, 1-127)")
+		self.clipping_label = tk.Label(self.clipping, font=self.font_small, text="Note Clipping Margin(seconds)")
+		# Resize frames if not big enough to hold widgets
 		self.info.pack_propagate(0)
 		self.input.pack_propagate(0)
 		self.output.pack_propagate(0)
 		self.convert.pack_propagate(0)
 		self.options.pack_propagate(0)
 		self.velocity.pack_propagate(0)
-		self.tracks.pack_propagate(0)
+		self.clipping.pack_propagate(0)
+		# Pack all of the Frames
 		self.convert.pack(side=tk.BOTTOM, expand=1, fill='both')
 		self.options.pack(side=tk.BOTTOM, expand=1, fill='both')
 		self.velocity.pack(side=tk.LEFT, expand=1, fill='both')
-		self.tracks.pack(side=tk.RIGHT, expand=1, fill='both')
+		self.clipping.pack(side=tk.RIGHT, expand=1, fill='both')
 		self.info.pack(side=tk.TOP, expand=1, fill='both')
 		self.input.pack(side=tk.LEFT, expand=1, fill='both')
 		self.output.pack(side=tk.RIGHT, expand=1, fill='both')
+		# Pack the title
 		self.title.pack(side=tk.TOP, expand=1, fill='y', pady=(10, 10))
 		self.convert_button.pack(side=tk.TOP, fill='both', expand=1, padx=(20, 20), pady=(20, 20))
+		# Pack the file selection buttons
 		self.input_button.pack(side=tk.TOP, fill='both', padx=(10, 10), pady=(10, 10))
 		self.output_button.pack(side=tk.TOP, fill='both', padx=(10, 10), pady=(10, 10))
+		# Pack the file entries
 		self.input_file_entry.pack(side=tk.TOP, fill='both', padx=(10, 10))
 		self.output_file_entry.pack(side=tk.TOP, fill='both', padx=(10, 10))
-		self.velocity_label.pack(side=tk.TOP, pady=(10	, 0))
+		# Pack the options
+		self.velocity_label.pack(side=tk.TOP, pady=(10, 0))
 		self.velocity_entry.pack(side=tk.BOTTOM, pady=(0, 10))
+		self.clipping_label.pack(side=tk.TOP, pady=(10, 0))
+		self.clipping_entry.pack(side=tk.BOTTOM, pady=(0, 10))
 
 	def convert_song(self):
 		# Parse the file
--- midi_parser.py
+++ midi_parser.py
@@ -9,27 +9,31 @@ def parse(input_file, output_file, velocity):
 	#    Initialization
 	# =====================
 
+	# Check if the input or output file wasn't given
+	if(input_file == "" or output_file == ""):
+		return
+
 	# Create lists for storing notes and meta messages
 	track_notes = []
 	track_meta = []
 
-	# Select the types of meta messages to be exported
-	meta_types = ["time_signature", "set_tempo", "track_name", "end_of_track"]
-
-	# Load the input file
+	# Load the input MIDI file
 	input_song = mido.MidiFile(input_file)
 
-	# Create a new file for the final song
+	# Create a new MIDI file for the final song
 	output_song = mido.MidiFile(ticks_per_beat=input_song.ticks_per_beat)
 
 	# Check if we should override the note velocity
-	new_velocity = 0
+	new_velocity = -1
+	
 	try:
+		# See if the user has input an integer
 		new_velocity = int(velocity)
 	except:
-		new_velocity = -1
+		pass
 
-	if(new_velocity < 0 or new_velocity > 127):
+	# If it's out of range, set it to -1
+	if(new_velocity < 1 or new_velocity > 127):
 		new_velocity = -1
 
 	# ==========================
@@ -48,7 +52,7 @@ def parse(input_file, output_file, velocity):
 		# Store all meta messages in a list
 		meta_messages = [msg for msg in track if msg.is_meta]
 
-		# Set the time to 0
+		# Create a time variable for storing absolute time and set it to 0
 		tickTime = 0
 
 		# Create a new empty list inside the main list for all tracks to append notes to
@@ -58,17 +62,11 @@ def parse(input_file, output_file, velocity):
 
 		# Loop through all meta messages
 		for msg in meta_messages:
-			# If the meta message is one of the specified types
-			if(msg.type in meta_types):
-				# Append it to the meta list for this track
-				track_meta[i].append(msg)
-			# If it is a track name message
-			if(msg.type == meta_types[2]):
-				# Apply the time offset
-				tickTime = msg.time
+			# Append the message to the meta list for this track
+			track_meta[i].append(msg)
 
 		# If this is just a meta track
-		if(len([msg for msg in track if msg.type == "note_on" or msg.type == "note_off"]) == 0):
+		if(len(note_messages) == 0):
 			# Create a new track
 			finished_track = mido.MidiTrack()
 			# Append the track to the output song
@@ -83,19 +81,25 @@ def parse(input_file, output_file, velocity):
 		#     Convert Note Format
 		# ===========================
 
-		sustain_last = False
+		# Create a variable to store the state of the sustain controller
 		sustain = False
-		sostenuto_last = False
+		# Create a variable to store the state of the sustain controller last loop
+		sustain_last = False
+		# Create a variable to store the state of the sostenuto controller
 		sostenuto = False
+		# Create a variable to store the state of the sostenuto controller last loop
+		sostenuto_last = False
+		# Create a variable to store all notes being sustained by the sostenuto controller
 		sostenuto_notes = []
+		# Create a variable to turn all notes off 
 		off = False
 
 		# Loop through all notes in the track
 		for j, msg in enumerate(note_messages):
-			# If this is the last note
+			# If this is the last message
 			if(j == len(note_messages) - 1):
-				# End all notes
-				off = True
+				off = False
+
 			# If there is a change in time then add that to the absolute time
 			tickTime += msg.time
 			# If this is a note on message
@@ -122,34 +126,76 @@ def parse(input_file, output_file, velocity):
 					if(track_notes[i][j][2] == msg.note and track_notes[i][j][1] == None):
 						# Add the current time as its end time
 						track_notes[i][j][1] = tickTime
+			# If this message is a controller change
 			if(msg.type == "control_change"):
+				# If it is a sustain message
 				if(msg.control == 64):
+					# Set it on/off
 					sustain = not msg.value < 64
+				# If it is a sostenuto message 
 				if(msg.control == 66):
+					# Set it on/off
 					sostenuto = not msg.value < 64
+				# If this is a controller message to turn all notes off
 				if(msg.control == 120):
+					# Turn on the flag
 					off = True
+				# If this is a reset controllers message
 				if(msg.control == 121):
+					# Turn off sustain and sostenuto
 					sustain = False
 					sostenuto = False
-			
-			if((sustain_last and not sustain) or off):
-				notes_on = list(filter(lambda e: e[1] == None and e[2] not in sostenuto_notes, track_notes[i]))
+
+			# If sostenuto just turned on
+			if(sostenuto and not sostenuto_last):
+				# Remove the notes it was holding down
+				sostenuto_notes.clear()
+				# Check which notes are being pressed
+				notes_on = list(filter(lambda e: e[1] == None, track_notes[i]))
+				# Loop through the notes
 				for note in notes_on:
+					# Add their pitch to the list
+					sostenuto_notes.append(note[2])
+
+			# If sustain has changed to off
+			if(sustain_last and not sustain):
+				# Check which notes are on and aren't being sustained by sostenuto
+				notes_on = list(filter(lambda e: e[1] == None and e[2] not in sostenuto_notes, track_notes[i]))
+				# Loop through them
+				for note in notes_on:
+					# Set their end time to now
 					note[1] = tickTime
 
-			if(sostenuto and not sostenuto_last):
-				sostenuto_notes.clear()
-				notes_on = list(filter(lambda e: e[1] == None, track_notes[i]))
-				for note in notes_on:
-					sostenuto_notes.append(note[2])
-					
+			# If sostenuto has been released
 			if(sostenuto_last and not sostenuto):
-				for note[2] in sostenuto_notes:
-					note[1] = tickTime
-			
+				# Loop through all notes
+				for note in track_notes[i]:
+					# Loop through all notes being sustained by sostenuto
+					for sostenuto_note in sostenuto_notes:
+						# If they have the same pitch and the note has no end time
+						if(note[2] == sostenuto_note and note[1] == None):
+							# Set its end time to now
+							note[1] = tickTime
+
+			# If there is an all notes off message
+			if(off):
+				# Loop through all notes
+				for note in track_notes[i]:
+					# If the note doesn't have an end time
+					if(note[1] == None):
+						# Set its end time to now
+						note[1] = tickTime
+				# Clear all sostenuto notes
+				sostenuto_notes.clear()
+				# Turn off sostenuto
+				sostenuto = False
+				sostenuto_last = False
+
+			# Set the current value of sustain to the last variable for the next loop
 			sustain_last = sustain
+			# Set the current value of sostenuto to the last variable for the next loop
 			sostenuto_last = sostenuto
+			# Turn the off flag off in case it was on
 			off = False
 
 		# =====================
@@ -164,8 +210,6 @@ def parse(input_file, output_file, velocity):
 
 		# Re-sort the list
 		track_notes[i].sort(key=lambda e: e[0])
-
-		lastTime = 0
 
 		# ======================
 		#    Track Splitting
@@ -182,9 +226,11 @@ def parse(input_file, output_file, velocity):
 			# Calculate the track index of the note
 			track_index = get_track_index(note, track_notes[i])
 
+			# If we need more tracks, add them
 			for j in range(0, 1 + track_index - len(new_tracks)):
 				new_tracks.append([])
 
+			# Add the note to its track
 			new_tracks[track_index].append(note)
 
 
@@ -193,28 +239,28 @@ def parse(input_file, output_file, velocity):
 		# ======================
 
 		for j, new_track in enumerate(new_tracks):
+			# Create a new track to append to the MIDI file that will be exported
 			finished_track = mido.MidiTrack()
 
+			# Add the track to the output file
 			output_song.tracks.append(finished_track)
 
-			lastTime = 0
+			# Re-use tickTime and set it to 0
+			tickTime = 0
 
+			# Set the finished track name to the old track name with an index starting with 1
 			finished_track.name = track.name + " " + str(j + 1)
 
-			new_track.sort(key=lambda e: e[0])
-
-			print("Track: " + finished_track.name)
-
+			# Loop through all of the notes in the new track
 			for note in new_track:
-				if(note[0] - lastTime < 0):
-					print("ON " + str(note))
-
-				if(note[1] - note[0] < 0):
-					print("OFF " + str(note))
-				finished_track.append(Message("note_on", note=note[2], velocity=new_velocity if new_velocity >= 0 else note[3], time=(note[0] - lastTime)))
+				# Add a note_on message for the note
+				finished_track.append(Message("note_on", note=note[2], velocity=new_velocity if new_velocity >= 0 else note[3], time=(note[0] - tickTime)))
+				# Add a note_off message for the note
 				finished_track.append(Message("note_off", note=note[2], velocity=0, time=(note[1] - note[0])))
-				lastTime = note[1]
+				# Set the absolute time to the last message added(the note_off message)
+				tickTime = note[1]
 
+	# Save the song
 	output_song.save(output_file)
 
 # Check if two notes overlap
@@ -234,13 +280,22 @@ def get_notes_above(note, notes):
 
 # Get the track index of a note
 def get_track_index(note, notes):
+	# Get the notes above this note
 	notes_above = get_notes_above(note, notes)
+	# Default the track index to 0
 	track_index = 0
+	# Create a list for storing the indices of the notes above this note
 	track_indices = []
+	# If there are no notes above this note
 	if(len(notes_above) == 0):
+		# It's track_index is 0
 		return track_index
+	# If there are notes above it, loop through them
 	for note in notes_above:
+		# Check their track index and add it to the list
 		track_indices.append(get_track_index(note, notes))
+	# Find the lowest number not in the list greater than 0
 	while track_index in track_indices:
 		track_index += 1
+	# Return it
 	return track_index
